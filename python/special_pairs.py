2e9c7449c20c01e5470716c81a95d5ca18a8f200
Chinedu Ekene Okpala <allstackdev@gmail.com>
2024-06-22 02:41:46 +0000

    Python
    First Commit - Special Pairs for Python

---
+++ python/special_pairs.py
@@ -0,0 +1,30 @@
+def is_prime(num):
+    if num <= 1:
+        return False
+    if num == 2:
+        return True
+    if num % 2 == 0:
+        return False
+    
+    for i in range(3, int(num**0.5) + 1, 2):
+        if num % i == 0:
+            return False
+    return True
+    
+def special_pairs(n):
+    primes = set()
+    res = []
+    
+    for i in range(1, n-1, 1):
+        if is_prime(i):
+            primes.add(i)
+    
+    # Find special pairs
+    for p in primes:
+        complement = n - p
+        if complement in primes and p < complement:
+            res.append([p, complement])
+    
+    return res
+
+print("Special Pairs - PY %s" % special_pairs(18))
