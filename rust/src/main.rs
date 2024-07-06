a3f55e0248c2aa7cc4921db19e7c0c95d4d70152
Chinedu Ekene Okpala <allstackdev@gmail.com>
2024-07-06 02:38:39 +0000

    tree constructor aglorithms

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,8 @@
 mod special_pairs;
+mod tree_constructor;
 
 fn main() {
     println!("Special Pairs - RS {:?}", special_pairs::get(18));
-
+    println!("Tree Constructor - RS {:?}", tree_constructor::get(vec!["(1,2)", "(2,4)", "(5,7)", "(7,2)", "(9,5)"]));
+    println!("Tree Constructor - RS {:?}", tree_constructor::get(vec!["(1,2)", "(3,2)", "(2,12)", "(5,2)"]));
 }
---
+++ rust/src/tree_constructor.rs
@@ -0,0 +1,38 @@
+use std::collections::HashMap;
+
+pub fn get(str_arr: Vec<&str>) -> bool {
+    let mut parents: HashMap<String, Vec<String>> = HashMap::new();
+    let mut children: HashMap<String, String> = HashMap::new();
+
+    for &pair in &str_arr {
+        let cleaned_pair = pair.replace(['(', ')'], "");
+        let nodes: Vec<_> = cleaned_pair.split(',').collect();
+        let child = nodes[0].to_owned();
+        let parent = nodes[1].to_owned();
+
+        // Insert the child into the parent's children list
+        parents
+            .entry(parent.clone())
+            .or_default()
+            .push(child.clone());
+
+        // If a parent has more than two children, it's not a valid binary tree
+        if parents[&parent].len() > 2 {
+            return false;
+        }
+
+        // If a child already has a parent, it's not a valid binary tree
+        if children.contains_key(&child) {
+            return false;
+        }
+
+        // Register the parent of the child
+        children.insert(child, parent);
+    }
+
+    let root_count = parents
+        .keys()
+        .filter(|&parent| !children.contains_key(parent))
+        .count();
+    root_count == 1
+}
